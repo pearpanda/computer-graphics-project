67b5b3a4c3a99c115db1726228b148d2b0be738b
Luka Jovicic <luka.jovicic16+git@gmail.com>
2020-11-24 22:48:40 +0100

    Add Court implementation (constructors and getters)

--- include/Court.h
+++ include/Court.h
@@ -2,16 +2,36 @@
 #define RG_COURT_H
 
 /**
- * Class for various configuration properties.
+ * Class for various configuration properties related to court. All properties
+ * are read-only.
  */
 class Court {
-public:
-    Court();
-    Court(float court_width, float hoop_radius);
+// This is a trivial class--it contains no logic, only plain constructors and
+// accessors, hence definition and implementation are not split and
+// private/public block order is swapped (public block is boilerplate).
+
 private:
     float court_width = 15;
     float court_length = 28;
     float hoop_radius = 0.229;
     //there's probably more config to be added here
+
+public:
+    Court() {}
+    Court(float court_width, float court_length, float hoop_radius) {
+        this->court_width = court_width;
+        this->court_length = court_length;
+        this->hoop_radius = hoop_radius;
+    }
+
+    float get_court_width() {
+        return court_width;
+    }
+    float get_court_length() {
+        return court_length;
+    }
+    float get_hoop_radius() {
+        return hoop_radius;
+    }
 };
 #endif // RG_COURT_H
