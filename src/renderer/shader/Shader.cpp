d8944ecd3028fe97ae0999b7ad8a50c9448f6ba2
Petar Nikić <mi18049@alas.matf.bg.ac.rs>
2020-12-05 20:57:24 +0100

    Correct typo

--- src/renderer/shader/Shader.cpp
+++ src/renderer/shader/Shader.cpp
@@ -69,7 +69,7 @@ int Shader::get_uniform_location(const std::string& name) const {
     int location = glGetUniformLocation(shader_id_, name.c_str());
     if (location == -1)
         spdlog::warn("RG::SHADER::GET_LOCATION: location for uniform {} is "
-                     "non-existant",
+                     "non-existent",
                      name);
     return location;
 }
