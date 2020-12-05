627f2afdde905d6de374b04ad4e2c21a458eb6d7
Petar Nikić <mi18049@alas.matf.bg.ac.rs>
2020-12-05 20:11:12 +0100

    Add Shader abstraction
    Implemented a Shader class with the following features:
    - Compiling and linking shader components (vertex, fragment)
    - Compilation and linking error logging (with `spdlog`)
    - Binding and unbinding
    - Uniform setting for common types

---
+++ include/rg/renderer/shader/Shader.hpp
@@ -0,0 +1,39 @@
+#ifndef RG_RENDERER_SHADER_SHADER_HPP
+#define RG_RENDERER_SHADER_SHADER_HPP
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat2x2.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <string>
+
+namespace rg {
+
+class Shader {
+public:
+    static Shader compile(const std::string& vertex_source,
+                          const std::string& fragment_source);
+    ~Shader();
+    void bind() const;
+    void unbind() const;
+
+    void set(const std::string& uniform, float value);
+    void set(const std::string& uniform, const glm::vec2& value);
+    void set(const std::string& uniform, const glm::vec3& value);
+    void set(const std::string& uniform, const glm::vec4& value);
+    void set(const std::string& uniform, const glm::mat2x2& value);
+    void set(const std::string& uniform, const glm::mat3x3& value);
+    void set(const std::string& uniform, const glm::mat4x4& value);
+private:
+    // NOLINTNEXTLINE(google-explicit-constructor)
+    Shader(unsigned int id);
+    int get_uniform_location(const std::string& name);
+    unsigned int shader_id_;
+};
+
+} // namespace rg
+
+#endif // RG_RENDERER_SHADER_SHADER_HPP
---
+++ src/renderer/shader/Shader_set.cpp
@@ -0,0 +1,57 @@
+#include <rg/renderer/shader/Shader.hpp>
+
+#include <glad/glad.h>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace rg {
+
+void Shader::set(const std::string& uniform, float value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniform1f(location, value);
+    }
+}
+
+void Shader::set(const std::string& uniform, const glm::vec2& value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniform2fv(location, 1, glm::value_ptr(value));
+    }
+}
+
+void Shader::set(const std::string& uniform, const glm::vec3& value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniform3fv(location, 1, glm::value_ptr(value));
+    }
+}
+
+void Shader::set(const std::string& uniform, const glm::vec4& value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniform4fv(location, 1, glm::value_ptr(value));
+    }
+}
+
+void Shader::set(const std::string& uniform, const glm::mat2x2& value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniformMatrix2fv(location, 1, GL_FALSE, glm::value_ptr(value));
+    }
+}
+
+void Shader::set(const std::string& uniform, const glm::mat3x3& value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(value));
+    }
+}
+
+void Shader::set(const std::string& uniform, const glm::mat4x4& value) {
+    int location = get_uniform_location(uniform);
+    if (location != -1) {
+        glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
+    }
+}
+
+};
