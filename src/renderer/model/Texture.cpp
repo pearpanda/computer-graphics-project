6a5b6d586a983762418139888936460a4eb63387
Petar Nikić <mi18049@alas.matf.bg.ac.rs>
2020-12-13 13:23:19 +0100

    Fix colours

--- src/renderer/model/Texture.cpp
+++ src/renderer/model/Texture.cpp
@@ -21,31 +21,16 @@ Texture::Texture(const std::string& path, TextureType type)
                     GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    int width, height, num_channels, comp;
-    auto* data = stbi_load(path.c_str(), &width, &height, &num_channels, 0);
+    int width, height, num_channels;
+
+    auto* data = stbi_load(path.c_str(), &width, &height, &num_channels, 4);
     if (data == nullptr) {
         spdlog::error(
                 "ERROR::RG::TEXTURE: Failed to load texture at path \"{}\"",
                 path);
     } else {
-        switch (num_channels) {
-            case 1:
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RED,
-                             GL_UNSIGNED_BYTE, data);
-                break;
-            case 2:
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RG8, width, height, 0, GL_RG,
-                             GL_UNSIGNED_BYTE, data);
-                break;
-            case 3:
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0,
-                             GL_RGB, GL_UNSIGNED_BYTE, data);
-                break;
-            default:
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0,
-                             GL_RGBA, GL_UNSIGNED_BYTE, data);
-                break;
-        }
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     stbi_image_free(data);
