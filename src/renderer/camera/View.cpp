ee6daf759dee5aaefc368157365a2b500240be36
Petar Nikić <mi18049@alas.matf.bg.ac.rs>
2020-12-20 14:33:39 +0100

    Update View misconceptions
    The critical issue is that the View class depends on the `glm` library,
    and it takes a __vertical field of view__ and an __aspect ratio__ for
    its projection calculations, whereas the `View` class sent
    `horizontal_fov` as the vertical FOV, and `vertical_fov` as the aspect
    ratio.
    
    Coincidentally, radian values of common angles for a field of view do
    not look bad as aspect ratios. :)

--- src/renderer/camera/View.cpp
+++ src/renderer/camera/View.cpp
@@ -4,11 +4,17 @@
 
 namespace rg {
 
+View::View()
+        : position{0.0f}, direction{0.0f, 0.0f, -1.0f}, up{0.0f, 1.0f, 0.0f},
+          vertical_fov{60.0f},
+          aspect_ratio{4.0f / 3.0f}, z_near{0.1f}, z_far{100.0f} {
+}
+
 View::View(glm::vec3 position, glm::vec3 direction, glm::vec3 up,
-           float horizontal_fov, float vertical_fov, float z_near, float z_far)
+           float vertical_fov, float aspect_ratio, float z_near, float z_far)
         : position{position}, direction{direction}, up{up},
-          horizontal_fov{horizontal_fov},
-          vertical_fov{vertical_fov}, z_near{z_near}, z_far{z_far} {
+          vertical_fov{vertical_fov},
+          aspect_ratio{aspect_ratio}, z_near{z_near}, z_far{z_far} {
 }
 
 glm::mat4 View::get_view_matrix() const {
@@ -16,8 +22,8 @@ glm::mat4 View::get_view_matrix() const {
 }
 
 glm::mat4 View::get_projection_matrix() const {
-    return glm::perspective(glm::radians(horizontal_fov),
-                            glm::radians(vertical_fov), z_near, z_far);
+    return glm::perspective(glm::radians(vertical_fov), aspect_ratio, z_near,
+                            z_far);
 }
 
 } // namespace rg
