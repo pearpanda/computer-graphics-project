809a2e406f144ae5c019832f7a41c40b396dfc02
Petar Nikić <mi18049@alas.matf.bg.ac.rs>
2020-12-20 14:39:03 +0100

    Update example
    The backpack example uses the new `Camera` system, with 4 cameras.
    It uses framebuffers to draw on the screen in one of two modes:
    1. Single camera view
    2. Four-way camera view
    
    The single camera view shows the scene as a single camera sees it.
    Switching between cameras on the fly is supported.
    
    The four-way camera view splits the window in quarters, and shows
    the scene from each camera in its own quarter.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+#include <rg/renderer/camera/Camera.hpp>
+#include <rg/renderer/camera/Surface.hpp>
 #include <rg/renderer/model/Model.hpp>
-#include <rg/renderer/camera/View.hpp>
 #include <rg/renderer/shader/Shader.hpp>
 #include <rg/util/read_file.hpp>
 
@@ -16,26 +17,96 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera
-rg::View* camera = nullptr;
+std::array<rg::Camera*, 4> cameras{nullptr};
+unsigned int active_camera = 0;
+bool multiple_cameras = false;
+bool enable_toggle_multiple_cameras = true;
+std::array<rg::Surface*, 4> surfaces{nullptr};
+
+float sensitivity = 0.003f;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-float sensitivity = 0.003f;
-float yaw = 0.0f, pitch = 0.0f;
 
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-int main()
-{
+void render(const rg::Shader& shader, const rg::Model& model,
+            const rg::Camera& camera, const rg::Surface& surface) {
+    // don't forget to enable shader before setting uniforms
+    shader.bind();
+    surface.bind();
+
+    glClearColor(0.2f, 0.4f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // view/projection transformations
+    glm::mat4 projection = camera.get_projection_matrix();
+    glm::mat4 view = camera.get_view_matrix();
+    shader.set("projection", projection);
+    shader.set("view", view);
+
+    // render the loaded model
+    glm::mat4 model_matrix = glm::mat4(1.0f);
+    model_matrix = glm::translate(
+            model_matrix,
+            glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the
+                                          // center of the scene
+    model_matrix = glm::scale(
+            model_matrix,
+            glm::vec3(1.0f, 1.0f, 1.0f)); // it's a bit too big for our scene,
+                                          // so scale it down
+    shader.set("model", model_matrix);
+    model.draw(shader);
+
+    surface.unbind();
+}
+
+// Positional rendering: in specific corner of the screen
+void render(const rg::Shader& shader, const rg::Surface& surface,
+            unsigned int index) {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    static const rg::Surface::SubViewDirectives no_transform{
+            glm::vec2{0.0f, 0.0f}, glm::vec2{1.0f, 1.0f}};
+    static const glm::vec2 surface_dimensions{1.0f, 1.0f};
+    static const std::array<glm::vec2, 4> origins{
+            glm::vec2{-0.5f, -0.5f},
+            glm::vec2{0.5f, -0.5f},
+            glm::vec2{0.5f, 0.5f},
+            glm::vec2{-0.5f, 0.5f},
+    };
+    static const std::array<rg::Surface::DrawDirectives, 4> directives{
+            rg::Surface::DrawDirectives{rg::Surface::ScreenDirectives{
+                                                origins[0], surface_dimensions},
+                                        no_transform},
+            rg::Surface::DrawDirectives{rg::Surface::ScreenDirectives{
+                                                origins[1], surface_dimensions},
+                                        no_transform},
+            rg::Surface::DrawDirectives{rg::Surface::ScreenDirectives{
+                                                origins[2], surface_dimensions},
+                                        no_transform},
+            rg::Surface::DrawDirectives{rg::Surface::ScreenDirectives{
+                                                origins[3], surface_dimensions},
+                                        no_transform}};
+
+    surface.draw(shader, directives[index]);
+}
+
+void render(const rg::Shader& shader, const rg::Surface& surface) {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    surface.draw(shader);
+}
+
+int main() {
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -61,21 +132,48 @@ int main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    camera = new rg::View{glm::vec3{0.0f, 0.0f, -3.0f},glm::vec3{0.0f, 0.0f, 1.0f}, glm::vec3{0.0f, 1.0f, 0.0f}, 60.0f, static_cast<float>(SCR_WIDTH) / SCR_HEIGHT * 60.0f, 0.1f, 100.0f};
-
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading
+    // model).
     stbi_set_flip_vertically_on_load(true);
+
+    // Initialize cameras and their surfaces
+    // -------------------------------------
+    cameras[0] = new rg::Camera{glm::vec3{-0.5f, -0.5f, 3.0f},
+                                glm::vec3{0.0f, 0.0f, -1.0f},
+                                60.0f,
+                                static_cast<float>(SCR_WIDTH) / SCR_HEIGHT,
+                                0.1f,
+                                100.0f};
+    cameras[1] = new rg::Camera{glm::vec3{0.5f, -0.5f, 3.0f},
+                                glm::vec3{0.0f, 0.0f, -1.0f},
+                                60.0f,
+                                static_cast<float>(SCR_WIDTH) / SCR_HEIGHT,
+                                0.1f,
+                                100.0f};
+    cameras[2] = new rg::Camera{glm::vec3{0.5f, 0.5f, 3.0f},
+                                glm::vec3{0.0f, 0.0f, -1.0f},
+                                60.0f,
+                                static_cast<float>(SCR_WIDTH) / SCR_HEIGHT,
+                                0.1f,
+                                100.0f};
+    cameras[3] = new rg::Camera{glm::vec3{-0.5f, 0.5f, 3.0f},
+                                glm::vec3{0.0f, 0.0f, -1.0f},
+                                60.0f,
+                                static_cast<float>(SCR_WIDTH) / SCR_HEIGHT,
+                                0.1f,
+                                100.0f};
+    for (unsigned int i = 0; i < 4; ++i)
+        surfaces[i] = new rg::Surface{SCR_WIDTH, SCR_HEIGHT};
 
     // configure global opengl state
     // -----------------------------
@@ -83,14 +181,20 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    auto* ourShader = new rg::Shader{rg::Shader::compile(rg::util::readFile(RESOURCE_DIRECTORY "/shaders/example.vs.glsl"), rg::util::readFile(RESOURCE_DIRECTORY "/shaders/example.fs.glsl"))};
+    auto* shader = new rg::Shader{rg::Shader::compile(
+            rg::util::readFile(RESOURCE_DIRECTORY "/shaders/example.vs.glsl"),
+            rg::util::readFile(RESOURCE_DIRECTORY "/shaders/example.fs.glsl"))};
+    auto* surface_shader = new rg::Shader{rg::Shader::compile(
+            rg::util::readFile(RESOURCE_DIRECTORY "/shaders/surface.vs.glsl"),
+            rg::util::readFile(RESOURCE_DIRECTORY "/shaders/surface.fs.glsl"))};
 
     // load models
     // -----------
-    auto* ourModel = new rg::Model(RESOURCE_DIRECTORY "/objects/backpack/backpack.obj");
+    auto* model =
+            new rg::Model(RESOURCE_DIRECTORY "/objects/backpack/backpack.obj");
 
     // draw in wireframe
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     // -----------
@@ -98,7 +202,7 @@ int main()
     {
         // per-frame time logic
         // --------------------
-        float currentFrame = glfwGetTime();
+        auto currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
@@ -111,32 +215,36 @@ int main()
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // don't forget to enable shader before setting uniforms
-        ourShader->bind();
+        if (multiple_cameras) {
+            glEnable(GL_DEPTH_TEST);
+            for (unsigned int i = 0; i < 4; ++i)
+                render(*shader, *model, *cameras[i], *surfaces[i]);
 
-        // view/projection transformations
-        glm::mat4 projection = camera->get_projection_matrix();
-        glm::mat4 view = camera->get_view_matrix();
-        ourShader->set("projection", projection);
-        ourShader->set("view", view);
+            glDisable(GL_DEPTH_TEST);
+            for (unsigned int i = 0; i < 4; ++i)
+                render(*surface_shader, *surfaces[i], i);
+        } else {
+            glEnable(GL_DEPTH_TEST);
+            render(*shader, *model, *cameras[active_camera],
+                   *surfaces[active_camera]);
 
-        // render the loaded model
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-        ourShader->set("model", model);
-        ourModel->draw(*ourShader);
+            glDisable(GL_DEPTH_TEST);
+            render(*surface_shader, *surfaces[active_camera]);
+        }
 
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse
+        // moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    delete ourModel;
-    delete camera;
-    delete ourShader;
+    delete model;
+    for (auto& camera : cameras)
+        delete camera;
+    for (auto& surface : surfaces)
+        delete surface;
+    delete shader;
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -146,20 +254,42 @@ int main()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
+void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
+    if (glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS) {
+        if (enable_toggle_multiple_cameras) {
+            multiple_cameras = !multiple_cameras;
+            enable_toggle_multiple_cameras = false;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_5) == GLFW_RELEASE) {
+        enable_toggle_multiple_cameras = true;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+        active_camera = 0;
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+        active_camera = 1;
+    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+        active_camera = 2;
+    if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS)
+        active_camera = 3;
+
+    auto camera = cameras[active_camera];
+    auto front = camera->get_direction();
+    auto right = camera->get_right();
+
     float speed = 2.0f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera->position += speed * camera->direction;
+        camera->move(speed * front);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera->position -= speed * camera->direction;
+        camera->move(-speed * front);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera->position -= speed * glm::cross(camera->direction, camera->up);
+        camera->move(-speed * right);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera->position += speed * glm::cross(camera->direction, camera->up);
+        camera->move(speed * right);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -173,33 +303,26 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    auto x_position = static_cast<float>(xpos);
+    auto y_position = static_cast<float>(ypos);
+    if (firstMouse) {
+        lastX = x_position;
+        lastY = y_position;
         firstMouse = false;
     }
 
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    float x_offset = x_position - lastX;
+    float y_offset = y_position - lastY;
 
-    lastX = xpos;
-    lastY = ypos;
+    lastX = x_position;
+    lastY = y_position;
 
-    float delta_yaw = sensitivity * (-xoffset);
-    float delta_pitch = sensitivity * (-yoffset);
+    float delta_yaw = sensitivity * (-x_offset);
+    float delta_pitch = sensitivity * (-y_offset);
 
-    yaw += delta_yaw;
-    pitch += delta_pitch;
-
-    pitch = glm::clamp(pitch, -glm::pi<float>() / 2.0f + 0.01f, glm::pi<float>() / 2.0f - 0.01f);
-    yaw = glm::mod(yaw, 2 * glm::pi<float>());
-
-    auto quat = glm::quat(glm::vec3{pitch, yaw, 0.0f});
-    camera->direction = quat * glm::vec3{0.0f, 0.0f, 1.0f};
-    camera->up = quat * glm::vec3{0.0f, 1.0f, 0.0f};
+    auto camera = cameras[active_camera];
+    camera->rotate(delta_yaw, delta_pitch);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
