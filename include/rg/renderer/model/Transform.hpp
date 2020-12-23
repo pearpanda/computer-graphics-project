c2153788d894c7997dcf73772cf0c533d3365257
Petar Nikić <mi18049@alas.matf.bg.ac.rs>
2020-12-23 02:08:31 +0100

    Add utility functions to Transform
    The Transform struct can now calculate local vectors, like "forward",
    "up" and "left".

--- include/rg/renderer/model/Transform.hpp
+++ include/rg/renderer/model/Transform.hpp
@@ -16,6 +16,24 @@ struct Transform {
     [[nodiscard]] glm::mat4 get_translate_matrix() const;
     [[nodiscard]] glm::mat4 get_rotate_matrix() const;
     [[nodiscard]] glm::mat4 get_scale_matrix() const;
+
+    [[nodiscard]] glm::vec3 get_forward_vector() const;
+    [[nodiscard]] glm::vec3 get_backward_vector() const;
+    [[nodiscard]] glm::vec3 get_up_vector() const;
+    [[nodiscard]] glm::vec3 get_down_vector() const;
+    [[nodiscard]] glm::vec3 get_left_vector() const;
+    [[nodiscard]] glm::vec3 get_right_vector() const;
+
+private:
+    // Canonical basis
+    static constexpr glm::vec3 LEFT{1.0f, 0.0f, 0.0f};
+    static constexpr glm::vec3 UP{0.0f, 1.0f, 0.0f};
+    static constexpr glm::vec3 FORWARD{0.0f, 0.0f, 1.0f};
+
+    // Extensions
+    static constexpr glm::vec3 RIGHT = -LEFT;
+    static constexpr glm::vec3 DOWN = -UP;
+    static constexpr glm::vec3 BACKWARD = -FORWARD;
 };
 
 } // namespace rg
--- src/renderer/model/Transform.cpp
+++ src/renderer/model/Transform.cpp
@@ -8,13 +8,13 @@ namespace rg {
 glm::mat4 Transform::get_model_matrix() const {
     glm::mat4 model{1.0f};
     model = glm::translate(model, position);
-    model = glm::mat4_cast(orientation) * model;
+    model = model * glm::mat4_cast(orientation);
     model = glm::scale(model, scale);
     return model;
 }
 
 glm::mat4 Transform::get_normal_matrix() const {
-    return glm::inverse(glm::mat3{get_model_matrix()});
+    return glm::transpose(glm::inverse(glm::mat3{get_model_matrix()}));
 }
 
 glm::mat4 Transform::get_translate_matrix() const {
@@ -27,6 +27,24 @@ glm::mat4 Transform::get_rotate_matrix() const {
 
 glm::mat4 Transform::get_scale_matrix() const {
     return glm::scale(glm::mat4{1.0f}, scale);
+}
+glm::vec3 Transform::get_forward_vector() const {
+    return orientation * FORWARD;
+}
+glm::vec3 Transform::get_backward_vector() const {
+    return orientation * BACKWARD;
+}
+glm::vec3 Transform::get_up_vector() const {
+    return orientation * UP;
+}
+glm::vec3 Transform::get_down_vector() const {
+    return orientation * DOWN;
+}
+glm::vec3 Transform::get_left_vector() const {
+    return orientation * LEFT;
+}
+glm::vec3 Transform::get_right_vector() const {
+    return orientation * RIGHT;
 }
 
 } // namespace rg
